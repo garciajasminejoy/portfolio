257e3b51eec9e28f247c0493db4b347a014de549
garciajasminejoy <garciajasminejoy@gmail.com>
2025-04-15 21:38:47 +0800

    feat: create page components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IntroComponent } from './components/intro/intro.component';
+import { WorkHistoryComponent } from "./components/work-history/work-history.component";
+import { TechStackComponent } from "./components/tech-stack/tech-stack.component";
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntroComponent],
+  imports: [RouterOutlet, IntroComponent, WorkHistoryComponent, TechStackComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

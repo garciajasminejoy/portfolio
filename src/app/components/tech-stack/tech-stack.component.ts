508113cec14369aac0efe28f0d447276f4c7a496
garciajasminejoy <garciajasminejoy@gmail.com>
2025-04-18 22:56:36 +0800

    feat: tech stack feature

--- src/app/components/tech-stack/tech-stack.component.ts
+++ src/app/components/tech-stack/tech-stack.component.ts
@@ -9,4 +9,54 @@ import { Component } from '@angular/core';
 })
 export class TechStackComponent {
 
+  list = [
+    {
+      name: 'Angular',
+      iconFileName: 'angular-icon.svg'
+    },
+    {
+      name: 'HTML',
+      iconFileName: 'html-5.svg'
+    },
+    {
+      name: 'CSS',
+      iconFileName: 'css-3.svg'
+    },
+    {
+      name: 'Typescript',
+      iconFileName: 'typescript-icon.svg'
+    },
+    {
+      name: 'NodeJS',
+      iconFileName: 'nodejs-icon.svg'
+    },
+    {
+      name: 'Jasmine',
+      iconFileName: 'jasmine.svg'
+    },
+    {
+      name: 'Laravel',
+      iconFileName: 'laravel.svg'
+    },
+    {
+      name: 'Git',
+      iconFileName: 'git-icon.svg'
+    },
+    {
+      name: 'Figma',
+      iconFileName: 'figma.svg'
+    },
+    {
+      name: 'Bootstrap',
+      iconFileName: 'bootstrap.svg'
+    },
+    {
+      name: 'NPM',
+      iconFileName: 'npm-icon.svg'
+    },
+    {
+      name: 'RxJS',
+      iconFileName: 'reactivex.svg'
+    },
+  ];
 }

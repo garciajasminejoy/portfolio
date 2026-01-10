244f7cca304b8d7d428facbcf8706d9eccdc395c
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-01-10 20:34:30 +0800

    feat: set colors

--- src/app/components/work-history/work-history.data.ts
+++ src/app/components/work-history/work-history.data.ts
@@ -4,7 +4,7 @@ export const WORK_HISTORY = [
         data: {
             name: 'Emapta Philippines',
             dateRange: 'Aug 2022 - Present',
-            description: 'lorem ipsum',
+            description: '',
             companyLogo: ''
         }
     },

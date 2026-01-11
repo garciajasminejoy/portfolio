c1d2b3f9cc86be2d45f3d0ed230ecd820fd3556d
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-01-11 20:19:23 +0800

    feat: favicon, email in contacts

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -5,34 +5,39 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
   socials = [
     {
+      name: 'Email',
+      url: 'mailto:garciajasminejoy@gmail.com',
+      iconFileName: 'at-symbol.svg',
+      accountName: 'garciajasminejoy@gmail.com',
+    },
+    {
       name: 'Github',
       url: 'https://github.com/garciajasminejoy',
       iconFileName: 'github.svg',
-      accountName: 'garciajasminejoy'
+      accountName: 'garciajasminejoy',
     },
     {
       name: 'LinkedIn',
       url: 'https://www.linkedin.com/in/jasmine-joy-garcia-904095163/',
       iconFileName: 'linkedin.svg',
-      accountName: 'Jasmine Joy Garcia'
+      accountName: 'Jasmine Joy Garcia',
     },
     {
       name: 'Instagram',
       url: 'https://www.instagram.com/hellojasgarcia/',
       iconFileName: 'instagram.svg',
-      accountName: 'hellojasgarcia'
+      accountName: 'hellojasgarcia',
     },
     {
       name: 'Facebook',
       url: 'https://www.facebook.com/hellojasgarcia/',
       iconFileName: 'fb.svg',
-      accountName: 'Jasmine Garcia'
+      accountName: 'Jasmine Garcia',
     },
   ];
-
 }
